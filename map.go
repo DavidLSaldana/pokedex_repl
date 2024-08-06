7a3a15ff73d8977e5b7bb3f918ebcc406758c0d2
DavidLSaldana <dleosaldana@gmail.com>
2024-08-06 08:08:19 -0700

    working base repl, then updated to use maps

---
+++ map.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func mapCommand(*c config) {
+
+	return
+}
+
+func mapBCommand() {
+	return
+}
---
+++ repl.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("pokedex > ")
+		scanner.Scan()
+
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		commandName := input[0]
+		command, exists := getCommands()[commandName]
+
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+type config struct {
+	NextUrl     string
+	PreviousUrl string
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+}
